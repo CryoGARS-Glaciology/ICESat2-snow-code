6c6155f7241553a58eed716258a559b65f5e5f64
Ellyn Enderlin <ellynenderlin@boisestate.edu>
2022-03-03 15:01:36 -0700

    Fixed numerous issues with footprints
    Fixed issues with slightly skewed footprints. Trimmed coregistration code to streamline. Fixed referencing to horizontally coregistred coordinates in vertical error estimation.

--- extract_icesat2_vertical_errors.m
+++ extract_icesat2_vertical_errors.m
@@ -1,4 +1,4 @@
-function [ztruth,differences,rmsez] = extract_icesat2_vertical_errors(icesat2, elevations, R2)
+function [ztruth,differences,rmsez] = extract_icesat2_vertical_errors(T, elevations, R2)
 % Function extract_icesat2_vertical_errors computes the vertical offsets
 % beteen horizontally-coregistered ICESat-2 tracks and a reference DTM
 % INPUTS: icesat2 = a csv file with icesat 2 elevations created using the
@@ -12,15 +12,20 @@ function [ztruth,differences,rmsez] = extract_icesat2_vertical_errors(icesat2, e
 %               rmsez = the root mean squared elevation difference between the 
 %                       icesat-2 footprints and corresponding DTM locations
 
-% Created 19 October 2020 by Colten Elkin (coltenelkin@u.boisestate.edu)
-% last modified 08 June 2021 by Ellyn Enderlin (ellynenderlin@boisestate.edu)
+% last modified 03 March 2022 by Ellyn Enderlin (ellynenderlin@boisestate.edu)
+
+%specify ICESat-2 footprint width & length
+footwidth = 11; % approx. width of icesat2 shot footprint in meters
+if contains(icesat2(1,:), 'ATL08') % ATL08 commands
+    default_length = 100; % approx. length of icesat2 shot footprint in meters
+elseif contains(icesat2(1,:), 'ATL06') % ATL06 commands
+    default_length = 40; % approx. length of icesat2 shot footprint in meters
+end
 
 %filter reference DTM elevations
 % elevations(elevations < -10) = nan; 
 elevations(elevations > 10000) = nan; % throw out bad data (clouds)
 
-%read ICESat-2 transect
-T = readtable(icesat2);
 %if you have vertically coregistered the ICESat2 data, use the adjusted elevations
 if ismember('Elevation_Coregistered', T.Properties.VariableNames)
     zmod = T.Elevation_Coregistered(:);
@@ -29,51 +34,39 @@ else
 end
 easts = T.Easting(:); % pull out the easting values
 norths = T.Northing(:); % pull out the northings
-footwidth = 11; % approx. width of icesat2 shot footprint in meters
 
 % initialize empty matrices
-theta = zeros(size(norths)); 
-xs = {};
-ys = {};
-xpoly = nan([1,5]);
-ypoly = nan([1,5]);
+theta = zeros(size(norths),2); 
 
 %create polygons of ICESat-2 footprints
 for r = 1:length(theta)
-    if r == length(theta)
-        theta(r) = abs(atan((norths(r) - norths(r-1))/(easts(r) - easts(r-1)))); % trig to get angle theta along-track
-    else
-        theta(r) = abs(atan((norths(r+1) - norths(r))/(easts(r+1) - easts(r)))); % trig to get angle theta along-track
+    %calculate the footprint orientation
+    if r == 1 %only angle pointed forwards
+        theta(r,1) = atan2d((norths(r+1)-norths(r)),(easts(r+1)-easts(r))); theta(r,2) = theta(r,1);
+        footlength = default_length;
+    elseif r == length(theta) %only angle pointed backwards
+        theta(r,1) = atan2d((norths(r)-norths(r-1)),(easts(r)-easts(r-1))); theta(r,2) = theta(r,1);
+        footlength = default_length;
+    else %calculate angles in each direction
+        theta(r,1) = atan2d((norths(r)-norths(r-1)),(easts(r)-easts(r-1)));
+        theta(r,2) = atan2d((norths(r+1)-norths(r)),(easts(r+1)-easts(r)));
+        footlength = sqrt((norths(r+1)-norths(r-1)).^2 + (easts(r+1)-easts(r-1)).^2)/2;
     end
     
-    if contains(icesat2, 'ATL08') % ATL08 commands   
-        % get the x and y vectors to form the polygon
-        xpoly(1) = easts(r) + (footwidth/2) - footwidth/2*cos((pi/2) - theta(r)); % calculate the 4 corners in the x direction
-        xpoly(2) = easts(r) + (footwidth/2) + footwidth/2*cos((pi/2) - theta(r));
-        xpoly(3) = easts(r) - (footwidth/2) + footwidth/2*cos((pi/2) - theta(r));
-        xpoly(4) = easts(r) - (footwidth/2) - footwidth/2*cos((pi/2) - theta(r));
-        xs{r} = [xpoly(1), xpoly(2), xpoly(3), xpoly(4), xpoly(1)]; % save the corners as a vector in the x-es cell array
-        
-        ypoly(1) = norths(r) - 50 - footwidth/2*cos((pi/2) - theta(r)); % calculate the 4 corners in the y direction
-        ypoly(2) = norths(r) - 50 + footwidth/2*cos((pi/2) - theta(r));
-        ypoly(3) = norths(r) + 50 + footwidth/2*cos((pi/2) - theta(r));
-        ypoly(4) = norths(r) + 50 - footwidth/2*cos((pi/2) - theta(r));
-        ys{r} = [ypoly(1), ypoly(2), ypoly(3), ypoly(4), ypoly(1)]; % save the corners as a vector in the y-s cell array
-        
-    elseif contains(icesat2, 'ATL06') % ATL06 commands
-        % get the x and y vectors to form the polygon
-        xpoly(1) = easts(r) + (footwidth/2) - footwidth/2*cos((pi/2) - theta(r)); % calculate the 4 corners in the x direction
-        xpoly(2) = easts(r) + (footwidth/2) + footwidth/2*cos((pi/2) - theta(r));
-        xpoly(3) = easts(r) - (footwidth/2) + footwidth/2*cos((pi/2) - theta(r));
-        xpoly(4) = easts(r) - (footwidth/2) - footwidth/2*cos((pi/2) - theta(r));
-        xs{r} = [xpoly(1), xpoly(2), xpoly(3), xpoly(4), xpoly(1)]; % save the corners as a vector in the x-es cell array
-        
-        ypoly(1) = norths(r) - 20 - footwidth/2*cos((pi/2) - theta(r)); % calculate the 4 corners in the y direction
-        ypoly(2) = norths(r) - 20 + footwidth/2*cos((pi/2) - theta(r));
-        ypoly(3) = norths(r) + 20 + footwidth/2*cos((pi/2) - theta(r));
-        ypoly(4) = norths(r) + 20 - footwidth/2*cos((pi/2) - theta(r));
-        ys{r} = [ypoly(1), ypoly(2), ypoly(3), ypoly(4), ypoly(1)]; % save the corners as a vector in the y-s cell array
-    end
+    %find box edges along the RGT
+    back_x = easts(r)-(footlength/2)*cosd(theta(r,1)); back_y = norths(r)-(footlength/2)*sind(theta(r,1));
+    front_x = easts(r)+(footlength/2)*cosd(theta(r,2)); front_y = norths(r)+(footlength/2)*sind(theta(r,2));
+    
+    %find box edges perpendicular to the centroid
+    xc(r,1) = easts(r)+(footwidth/2)*cosd(nanmean(theta(r,:))+90); yc(r,1) = norths(r)+(footwidth/2)*sind(nanmean(theta(r,:))+90);
+    xc(r,4) = easts(r)+(footwidth/2)*cosd(nanmean(theta(r,:))-90); yc(r,4) = norths(r)+(footwidth/2)*sind(nanmean(theta(r,:))-90);
+    
+    %solve for corner coordinates
+    xc(r,2) = back_x+(footwidth/2)*cosd(theta(r,1)+90); yc(r,2) = back_y+(footwidth/2)*sind(theta(r,1)+90);
+    xc(r,3) = back_x+(footwidth/2)*cosd(theta(r,1)-90); yc(r,3) = back_y+(footwidth/2)*sind(theta(r,1)-90);
+    xc(r,5) = front_x+(footwidth/2)*cosd(theta(r,2)-90); yc(r,5) = front_y+(footwidth/2)*sind(theta(r,2)-90);
+    xc(r,6) = front_x+(footwidth/2)*cosd(theta(r,2)+90); yc(r,6) = front_y+(footwidth/2)*sind(theta(r,2)+90);
+    clear back_* front_*;
 end
 
 %define the reference elevation data
@@ -84,21 +77,18 @@ else
     y = R2.YWorldLimits(1)+0.5*R2.CellExtentInWorldY:R2.CellExtentInWorldY:R2.YWorldLimits(2)-0.5*R2.CellExtentInWorldY; 
 end
 [xgrid, ygrid] = meshgrid(x, y); % create grids of each of the x and y coords
-elevation_report = zeros([1, length(xs)]);
+elevation_report = zeros([1, length(xc)]);
 
 %identify the reference elevation points in each ICESat2 footprint
-for t = 1:length(xs)
-    xv = xs{t}; % bounding box x vector
-    yv = ys{t}; % bounding box y vector
+for k = 1:length(xc)
+    xv = [xc(k,:) xc(k,1)]; % bounding box x vector
+    yv = [yc(k,:) yc(k,1)]; % bounding box y vector
     
-    % first trimming
+    %data in the footprint
     in = inpolygon(xgrid, ygrid, xv, yv); % get logical array of in values
-    pointsinx = xgrid(in); % save x locations
-    pointsiny = ygrid(in); % save y locations
-    elevationsin = elevations(in); % save elevations
-    elevation_report(t) = nanmean(elevationsin); %NEED TO MODIFY... 
-    %%FIGURE OUT HOW TO WEIGHT ACROSS-TRACK & HOW TO FIT A SLOPE LIKE
-    %%h_te_best_fit VARIABLE SAVED WITH ATL08
+    elevationsin = elevations(in); % isolate elevations within the footprint
+    elevation_report(k) = nanmean(elevationsin); %export the mean
+    clear xv yv in elevationsin;
 end
 ztruth = elevation_report(:);
 
